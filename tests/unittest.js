e7968e34d82d37f92bfc6b508efe4159c2326992
Mario Saldinger <mariosaldinger@gmail.com>
2011-12-22 14:41:48 +0000

    function literal

--- tests/unittest.js
+++ tests/unittest.js
@@ -1,30 +1,30 @@
 var ol = document.getElementById( 'tests' );
 
-function assert( condition, description ) {
+var assert = function( condition, description ) {
     var li = document.createElement( 'li' );
     var span = document.createElement( 'span' );
 
-    if ( !condition ) {
-        li.className = 'fail';
-        span.appendChild( document.createTextNode( 'FAIL: ' ) );
-        li.appendChild( span );
-        li.appendChild( document.createTextNode( description ) );
-    }
-    else {
+    if ( condition ) {
         li.className = 'pass';
         li.appendChild( span );
         span.appendChild( document.createTextNode( 'PASS' ) );
     }
+    else {
+        li.className = 'fail';
+        li.appendChild( span );
+        span.appendChild( document.createTextNode( 'FAIL: ' ) );
+        li.appendChild( document.createTextNode( description ) );
+    }
     ol.appendChild( li );
-}
+};
 
-function assertEquals( expected, actual, description ) {
+var assertEquals = function( expected, actual, description ) {
     assert(
         expected == actual,
         description + ': expected "' + expected + '", saw "' + actual + '"'
     );
-}
+};
 
-function assertCloseEnough( expected, actual, description ) {
+var assertCloseEnough = function( expected, actual, description ) {
     assertEquals( Math.round( 10000 * expected ), Math.round( 10000 * actual ), description );
-}
+};
